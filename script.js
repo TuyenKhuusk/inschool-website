3cdaaa8e8da5329fbf4a2a1928041cedc5166b58
TuyenKhuusk <149036024+TuyenKhuusk@users.noreply.github.com>
2025-09-25 16:01:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
     const registrationForm = document.getElementById('registrationForm');
+    const loginFormContainer = document.querySelector('.login-form-container');
+    const registrationFormContainer = document.querySelector('.signup-form');
+    const showLoginFormLink = document.getElementById('showLoginForm');
+    const showRegistrationFormLink = document.getElementById('showRegistrationForm');
 
+    // Mở/đóng form đăng nhập và đăng ký
+    showLoginFormLink.addEventListener('click', (e) => {
+        e.preventDefault();
+        registrationFormContainer.style.display = 'none';
+        loginFormContainer.style.display = 'block';
+    });
+
+    showRegistrationFormLink.addEventListener('click', (e) => {
+        e.preventDefault();
+        registrationFormContainer.style.display = 'block';
+        loginFormContainer.style.display = 'none';
+    });
+
+    // Xử lý đăng ký tài khoản
     registrationForm.addEventListener('submit', async (e) => {
         e.preventDefault();
 
@@ -10,22 +28,19 @@ document.addEventListener('DOMContentLoaded', () => {
         const confirmPassword = document.getElementById('confirmPassword').value;
 
         // Định nghĩa biểu thức chính quy để kiểm tra mật khẩu
-        // Mật khẩu phải có: ít nhất 8 ký tự, 1 chữ hoa, 1 chữ thường, 1 số và 1 ký tự đặc biệt
         const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
-        // Kiểm tra mật khẩu có đáp ứng yêu cầu không
         if (!passwordRegex.test(password)) {
             alert('Lỗi: Mật khẩu phải có ít nhất 8 ký tự, bao gồm chữ in hoa, chữ thường, số và ký tự đặc biệt.');
             return;
         }
 
-        // Kiểm tra mật khẩu và xác nhận mật khẩu có khớp không
         if (password !== confirmPassword) {
             alert('Lỗi: Mật khẩu và xác nhận mật khẩu không khớp. Vui lòng thử lại!');
             return;
         }
 
-        // Gửi dữ liệu đến Netlify Function
+        // Gửi dữ liệu đến Netlify Function để lưu trữ
         try {
             const response = await fetch('/.netlify/functions/register', {
                 method: 'POST',
@@ -35,10 +50,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 body: JSON.stringify({ name, email, password }),
             });
 
-            const result = await response.json();
-
             if (response.ok) { // Kiểm tra trạng thái HTTP của response
-                alert(`Chào mừng ${name}! Bạn đã đăng ký thành công. Bây giờ bạn có thể đăng nhập.`);
+                alert(`Chào mừng ${name}! Bạn đã đăng ký thành công.`);
                 window.location.href = 'welcome.html';
             } else {
                 alert('Đăng ký thất bại. Vui lòng thử lại.');
